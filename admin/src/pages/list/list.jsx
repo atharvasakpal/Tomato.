ce470b8964ebc6f16f702adcef59fb9192f8c1f4
Atharva Sakpal <atharvasakpal14@gmail.com>
2024-09-05 01:27:57 +0530

    list foods

--- admin/src/pages/list/list.jsx
+++ admin/src/pages/list/list.jsx
@@ -12,7 +12,7 @@ const List = () => {
     const response  = await axios.get(`${url}/api/food/list`);
     console.log(response.data);
     if(response.data.success){
-      setList(response.data.data);
+      setList(response.data.message);
     }
     else{
       toast.error("ERROR!")
@@ -24,7 +24,30 @@ const List = () => {
   },[])
 
   return (
-    <div>
+    <div className='list add flex-col'>
+      <p>All Foods List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+            <b>Image</b>
+            <b>Name</b>
+            <b>Category</b>
+            <b>Price</b>
+            <b>Action</b>
+        </div>
+        
+        {list.map((item,index)=>{
+               return(
+                <div key={index} className='list-table-format'>
+                  <img src={`${url}/images/`+item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>{item.category}</p>
+                  <p>{item.price}</p>
+                  <p>X</p>
+                </div>
+               )
+        })}
+
+      </div>
       
     </div>
   )
